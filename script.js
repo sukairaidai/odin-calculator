0d0210068d5f75ab3544f8883211cf9fe21394ba
Jean <adiariakus@gmail.com>
2024-10-31 15:17:24 -0500

    Operate based on operator

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let firstValue;
+let secondValue;
+let operator;
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            if (b === 0) {
+                return "error, division by zero";
+            }
+            return divide(a, b);
+        default:
+            return "error, unkown operation";
+    }
+}
